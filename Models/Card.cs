fa002f0820fcc26da82127abd99f4b95992a37d6
TKhan2000 <khatamjid314@gmail.com>
2023-09-14 19:45:20 -0700

    Added Bootswatch theme and Card model

---
+++ Models/Card.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YGWeb.Models
+{
+    public class Card
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string type { get; set; }
+
+        public int atk { get; set; }
+
+        public int def { get; set; }
+        [Range(1,12)]
+        public int level { get; set; }
+
+        public string race { get; set; }
+
+        public string attribute { get; set; }
+
+        public string archetype { get; set; }
+        public string description { get; set; }
+
+        public string imageURL { get; set; }
+
+    }
+}
