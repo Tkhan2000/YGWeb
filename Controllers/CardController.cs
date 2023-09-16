158fda6b53ae2d692dd53257c7632cf8184b06f5
TKhan2000 <khatamjid314@gmail.com>
2023-09-16 16:46:55 -0700

    Implemented Basic Search Functions

--- Controllers/CardController.cs
+++ Controllers/CardController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using YGWeb.Data;
 using YGWeb.Models;
+using PagedList;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace YGWeb.Controllers
 {
@@ -12,10 +14,28 @@ namespace YGWeb.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
+        public IActionResult Index(string searchString, string cardType)
         {
             IEnumerable<Card> objCardList = _db.Cards;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                objCardList = objCardList.Where(card => card.name.Contains(searchString) || card.description.Contains(searchString));
+            }
+            if (!String.IsNullOrEmpty(cardType))
+            {
+                objCardList = objCardList.Where(card => card.type.Contains(cardType));
+            }
             return View(objCardList);
+        }
+
+        public IActionResult SelectCardType() 
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "Monster", Value = "1" });
+            items.Add(new SelectListItem { Text = "Spell", Value = "2" });
+            items.Add(new SelectListItem { Text = "Trap", Value = "3" });
+            ViewBag.CardType = items;
+            return View(); 
         }
     }
 }
--- Models/Card.cs
+++ Models/Card.cs
@@ -4,6 +4,50 @@ namespace YGWeb.Models
 {
     public class Card
     {
+        public enum cardType
+        {
+            Monster,
+            Spell,
+            Trap
+        }
+        public enum cardRace
+        {
+            Aqua, 
+            Beast, 
+            BeastWarrior, 
+            Cyberse, 
+            Dinosaur, 
+            DivineBeast, 
+            Dragon, 
+            Fairy, 
+            Fiend, 
+            Fish, 
+            Insect, 
+            Machine, 
+            Plant, 
+            Psychic, 
+            Pyro, 
+            Reptile, 
+            Rock, 
+            SeaSerpent, 
+            Spellcaster, 
+            Thunder, 
+            Warrior, 
+            WingedBeast, 
+            Wyrm, 
+            Zombie
+        }
+
+        public enum cardAttribute
+        {
+            Dark, 
+            Divine, 
+            Earth, 
+            Fire, 
+            Light, 
+            Water, 
+            Wind
+        }
         [Required]
         public int id { get; set; }
         [Required]
