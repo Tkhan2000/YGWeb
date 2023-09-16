f263408f651de26911647c3c9f07a7299342772a
TKhan2000 <khatamjid314@gmail.com>
2023-09-15 19:55:34 -0700

    Added Entity Framework Packages and configured card database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YGWeb.Models;
+
+namespace YGWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
---
+++ Migrations/20230916025322_AddCardsToDatabase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YGWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCardsToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    atk = table.Column<int>(type: "int", nullable: false),
+                    def = table.Column<int>(type: "int", nullable: false),
+                    level = table.Column<int>(type: "int", nullable: false),
+                    race = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    attribute = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    archetype = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    desc = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    image_url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
